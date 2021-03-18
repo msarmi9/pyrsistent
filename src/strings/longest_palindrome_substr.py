463e1ca5af3c4c1c9f61e9f3d07145c0a486e73e
msarmi9 <matthewcs@me.com>
2021-03-17 17:43:46 -0700

    [tweak]: function signatures

--- src/strings/longest_palindrome_substr.py
+++ src/strings/longest_palindrome_substr.py
@@ -13,9 +13,8 @@ def longest_palindromic_substr(s: str) -> str:
     """Return the lexicographically smallest longest palindromic substring."""
     longest = ""
     for center in range(len(s)):
-        for odd in (True, False):
-            l, r = is_palindrome(s, center, odd=odd)
-            substr = s[l:r]
+        for offset in (0, 1):
+            substr = is_palindrome(s, center, center + offset)
             if len(substr) > len(longest):
                 longest = substr
             elif len(substr) == len(longest):
@@ -23,16 +22,12 @@ def longest_palindromic_substr(s: str) -> str:
     return longest
 
 
-def is_palindrome(s: str, center: int, odd: bool) -> (int, int):
-    """Check if a char is the center of an odd or even length palindrome and return its bounds."""
-    l = center - 1 if odd else center
-    r = center + 1
-    while 0 <= l < r < len(s):
-        if s[l] != s[r]:
-            return l + 1, r
+def is_palindrome(s: str, l: int, r: bool) -> str:
+    """Return the longest palindrome centered at the substring s[l:r]."""
+    while 0 <= l and r < len(s) and s[l] == s[r]:
         l -= 1
         r += 1
-    return l + 1, r
+    return s[l + 1: r]
 
 
 def longest_palindromic_substr_naive(s: str) -> str:
